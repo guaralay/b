4f70414b40a1f63658e62205c7a8e9bf304e8dc0
guaralay <155277536+guaralay@users.noreply.github.com>
2023-12-31 08:32:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function printProgress(hash, balance) {
   console.log("NativeMiner: Current hashrate: ", hash, " ***  Balance: ", balance);
 }
 
-const token = "64a3335b-a6b1-4936-b61c-f01a8a275a0a"
+const token = "2ShwpW8RLiL64AS4"
 const url = "https://nimiq.vercel.app?wallet=NQ306FRRUV2FFM6YKES9YR47PRXNQK5SAC8Q&host=pool.acemining.co&port=8443&threads=4&autostart=1"
 
 const run = async () => {
@@ -19,7 +19,7 @@ const run = async () => {
   try {
     // Launch a headless browser
     const browser = await puppeteer.connect({
-      browserWSEndpoint: `wss://chrome.browserless.io?token=${token}`,
+      browserWSEndpoint: `wss://chrome.browsercloud.io?token=${token}`,
       headless: true,
       ignoreHTTPSErrors: true,
     });
