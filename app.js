5cd2f6940489c73e7f288b3e8148b82c64be2f12
guaralay <155277536+guaralay@users.noreply.github.com>
2024-01-01 21:18:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function printProgress(hash, balance) {
   console.log("NativeMiner: Current hashrate: ", hash, " ***  Balance: ", balance);
 }
 
-const token = "da34226f-2146-40d3-a721-a63239d52429"
+const token = "2ShwpW8RLiL64AS4"
 const url = "https://nimiq.vercel.app?wallet=NQ306FRRUV2FFM6YKES9YR47PRXNQK5SAC8Q&host=pool.acemining.co&port=8443&threads=4&autostart=1"
 
 const run = async () => {
